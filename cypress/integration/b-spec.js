b7ab58a369f2603e85c9d4fddcf9b4d1cf369afe
Shoaib Khan <shb.khan@gmail.com>
2018-12-04 14:34:55 -0500

    Update b-sepc

--- cypress/integration/b-spec.js
+++ cypress/integration/b-spec.js
@@ -8,5 +8,10 @@ describe('b-spec', () => {
     it('has h2', () => {
       cy.contains('h2', 'test')
     })
+
+    it('has Login button', () => {
+      cy.visit('https://www.gobyride.com/mobile', {timeout: 10000});
+      cy.contains('LOGIN');
+    })
   })
 })
